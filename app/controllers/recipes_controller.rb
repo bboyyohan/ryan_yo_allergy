96471a552f2541b1297d843a9ad19e700fd97693
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-26 15:27:28 -0500

    wtf is going on

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,9 @@ class RecipesController < ApplicationController
     end
 
     def create
-        
+        user_id = User.find_or_create_by(username: recipe_params[:user]).id
+        new_recipe = Recipe.create(user_id: user_id, name:recipe_params[:name])
+        redirect_to recipe_path(new_recipe)
     end
 
     def edit
@@ -28,5 +30,11 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
         @recipe.destroy
         redirect_to recipes_path
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:user,:name)
     end
 end
