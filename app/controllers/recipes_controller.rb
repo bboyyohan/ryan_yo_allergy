34b8387c1e09414d8cb82908f11cdde28e87f216
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-26 12:59:21 -0500

    working on forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,15 @@
 class RecipesController < ApplicationController
 
     def index
-        
+        @recipes = Recipe.all
     end
 
     def show
-        
+        @recipe = Recipe.find(params[:id])
     end
 
     def new
-        
+        @recipe = Recipe.new
     end
 
     def create
