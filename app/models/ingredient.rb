a7b8809ea605a345df0e17bfe2c1ca1ee07b8c58
github username <github email address>
2020-02-26 12:29:13 -0500

    added seed data

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
     has_many :allergies
     has_many :users, through: :allergy
-    has_many :
-
+    has_many :recipe_ingredients
+    has_many :recipes, through: :recipe_ingredients
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 
-Ryan = User.create(username: "Ryan")
-Yo = User.create(username: "Yo")
+ryan = User.create(username: "Ryan")
+yo = User.create(username: "Yo")
 
-Ramen = Recipe.create(name: "Ramen", user_id: 1, ingredient_id: 2)
+ramen = Recipe.create(name: "Ramen", user_id: 1)
+pizza = Recipe.create(name: "Pizza", user_id: 2)
+burger = Recipe.create(name: "Burger", user_id: 1)
+taco = Recipe.create(name: "Taco", user_id: 2)
+spaghetti = Recipe.create(name: "Spaghetti", user_id: 1)
+
+noodle = Ingredient.create(name: "Noodle")
+tomato = Ingredient.create(name: "Tomato")
+cheese = Ingredient.create(name: "Cheese")
+milk = Ingredient.create(name: "Milk")
+beef = Ingredient.create(name: "Beef")
+
+a1 = Allergy.create(user_id: ryan.id, ingredient_id: beef.id)
+a2 = Allergy.create(user_id: yo.id, ingredient_id: cheese.id)
+a3 = Allergy.create(user_id: yo.id, ingredient_id: tomato.id)
+a1 = Allergy.create(user_id: ryan.id, ingredient_id: tomato.id)
+
+r1 = RecipeIngredient.create(recipe_id: ramen.id, ingredient_id: noodle.id)
+r2 = RecipeIngredient.create(recipe_id: burger.id, ingredient_id: beef.id)
+r3 = RecipeIngredient.create(recipe_id: ramen.id, ingredient_id: beef.id)
+r4 = RecipeIngredient.create(recipe_id: burger.id, ingredient_id: cheese.id)
+r5 = RecipeIngredient.create(recipe_id: taco.id, ingredient_id: tomato.id)
+r6 = RecipeIngredient.create(recipe_id: taco.id, ingredient_id: beef.id)
+
